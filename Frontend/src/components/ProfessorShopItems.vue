1ba47514ee5101290b65ed90d553b4e48dc9ac75
Bryne <babasalancebryne@gmail.com>
2025-06-07 06:03:13 +0800

    Update

--- Frontend/src/components/ProfessorShopItems.vue
+++ Frontend/src/components/ProfessorShopItems.vue
@@ -93,7 +93,7 @@
                     data-bs-toggle="modal"
                     data-bs-target="#updateItem"
                     @click="setUpdateItemData(item)"
-                    src="../assets/Prof-Class/Create.png"
+                    src="../assets/Prof-Class/create.png"
                     alt="update icon"
                   />
                   <img
